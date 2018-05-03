e4f328afe92e2f75d903e207a3857b466a0e2a7c
barisgecer <b.gecer@imperial.ac.uk>
2018-05-03 15:47:16 +0100

    align get_dataset fix

--- src/align/align_dataset_mtcnn.py
+++ src/align/align_dataset_mtcnn.py
@@ -37,6 +37,41 @@ import random
 import imageio
 from time import sleep
 
+class ImageClass():
+    "Stores the paths to images for a given class"
+
+    def __init__(self, name, image_paths):
+        self.name = name
+        self.image_paths = image_paths
+
+    def __str__(self):
+        return self.name + ', ' + str(len(self.image_paths)) + ' images'
+
+    def __len__(self):
+        return len(self.image_paths)
+
+def get_dataset(paths, has_class_directories=True):
+    dataset = []
+    for path in paths.split(';'):
+        path_exp = os.path.expanduser(paths)
+        classes = os.listdir(path_exp)
+        classes.sort()
+        nrof_classes = len(classes)
+        for i in range(nrof_classes):
+            class_name = classes[i]
+            facedir = os.path.join(path_exp, class_name)
+            image_paths = get_image_paths(facedir)
+            dataset.append(ImageClass(class_name, image_paths))
+
+    return dataset
+
+def get_image_paths(facedir):
+    image_paths = []
+    if os.path.isdir(facedir):
+        images = os.listdir(facedir)
+        image_paths = [os.path.join(facedir, img) for img in images]
+    return image_paths
+
 def main(args):
     sleep(random.random())
     output_dir = os.path.expanduser(args.output_dir)
@@ -45,7 +80,7 @@ def main(args):
     # Store some git revision info in a text file in the log directory
     src_path,_ = os.path.split(os.path.realpath(__file__))
     facenet.src.facenet.store_revision_info(src_path, output_dir, ' '.join(sys.argv))
-    dataset = facenet.src.facenet.get_dataset(args.input_dir)
+    dataset = get_dataset(args.input_dir)
     
     print('Creating networks and loading parameters')
     
